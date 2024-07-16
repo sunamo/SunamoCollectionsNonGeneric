48c055b4f26e5e03938a1732e96b80c339c6ca07
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 09:25:26 +0200

    Before removing all *SunamoExceptions.cs

--- _sunamo/SunamoExceptions/OnlyInSE/Exc.cs
+++ _sunamo/SunamoExceptions/OnlyInSE/Exc.cs
@@ -1,9 +1,5 @@
 namespace SunamoCollectionsNonGeneric._sunamo.SunamoExceptions.OnlyInSE;
 
-
-
-
-
 internal class Exc
 {
     
--- _sunamo/SunamoStringGetLines/SHGetLines.cs
+++ _sunamo/SunamoStringGetLines/SHGetLines.cs
@@ -1,4 +1,5 @@
 namespace SunamoCollectionsNonGeneric._sunamo.SunamoStringGetLines;
+
 internal class SHGetLines
 {
     internal static List<string> GetLines(string p)
