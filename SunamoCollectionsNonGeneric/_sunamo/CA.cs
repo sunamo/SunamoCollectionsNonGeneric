1db482b684cd0aff3011284daad5ddbd295130a7
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 01:05:18 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoCollectionsNonGeneric/_sunamo/CA.cs
+++
@@ -1,7 +0,0 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoCollectionsNonGeneric._sunamo;
-
-internal class CA
-{
-}
--- SunamoCollectionsNonGeneric/_sunamo/FS.cs
+++
@@ -1,8 +0,0 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoCollectionsNonGeneric._sunamo;
-
-//namespace SunamoCollectionsNonGeneric._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FS
-{
-}
